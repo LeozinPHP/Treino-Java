7c0b6fea520bf72c740db503f3148a91e581c8ee
LeozinPHP <125416873+LeozinPHP@users.noreply.github.com>
2025-02-20 15:24:20 -0300

    Add files via upload

---
+++ Exercicio2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        Scanner Consumo = new Scanner(System.in);
+        System.out.println("Digite a quilometragem rodada do seu carro");
+        int rodada = Consumo.nextInt();
+
+        System.out.println("Quantos litros tem no seu tanque?");
+        int tanque = Consumo.nextInt();
+
+        System.out.println("a")
+
+
+    }
+}
