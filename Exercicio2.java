2d7ccc3783bb5460fbbae4682ceb8aa6070b9e39
LeozinPHP <125416873+LeozinPHP@users.noreply.github.com>
2025-02-20 23:19:47 -0300

    Update Exercicio2.java

--- Exercicio2.java
+++ Exercicio2.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 public class Exercicio2 {
     public static void main(String[] args) {
         Scanner Consumo = new Scanner(System.in);
-        System.out.println("Digite a quilometragem rodada do seu carro");
+        System.out.println("Digite Distância total percorrida (EM KM)");
         int rodada = Consumo.nextInt();
 
-        System.out.println("Quantos litros tem no seu tanque?");
+        System.out.println("Digte o Total de combustível gasto");
         int tanque = Consumo.nextInt();
 
-        System.out.println("a")
+        System.out.println("A média de consumo é de " + rodada / + tanque);
 
-
+        Consumo.close();
     }
 }
